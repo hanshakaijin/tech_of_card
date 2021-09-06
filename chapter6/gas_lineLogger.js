a7be3b892b864789050d2ba85261c37b3111dd4c
noir_k <jeannenoir17@gmail.com>
2021-09-06 10:10:30 +0900

    chapter6のGASコードを変更 ・不要な変数の削除 ・変数定義の最適化（let→const） ・時刻取得方式の変更（自力→Utilities利用）

--- chapter6/gas_lineLogger.js
+++ chapter6/gas_lineLogger.js
@@ -15,11 +15,9 @@ function doPost(e) {
     console.log(events);
 
     events.forEach(function (event) {
-        const replyToken = event.replyToken;
-        
         const userId = event.source.userId;
-        let groupId = event.source.groupId || event.source.roomId || '-';
-        let userMessage = event.message.text;
+        const groupId = event.source.groupId || event.source.roomId || '-';
+        const userMessage = event.message.text;
 
         if (event.type == "message") {
             pushLogs(userId, groupId, userMessage);
@@ -34,28 +32,9 @@ function doPost(e) {
 }
 
 function pushLogs(userId, groupId, userMessage) {
-    const nowTimestamp = getCurrentTime();
+    const now = new Date();
+    const nowTimestamp = Utilities.formatDate(now, 'JST', 'yyyy/MM/dd HH:mm:ss');
     const spreadsheet = SpreadsheetApp.openById(LOG_SPREADSHEET_ID);
     const sheet = spreadsheet.getSheets()[LOG_WORKSHEET_ID];
     sheet.appendRow([nowTimestamp, userId, groupId, userMessage]);
-}
-
-//現在時刻取得（yyyy/mm/dd hh:mm:ss）
-function getCurrentTime() {
-    const now = new Date();
-    const res = "" + now.getFullYear() + "/" + padZero(now.getMonth() + 1) +
-        "/" + padZero(now.getDate()) + " " + padZero(now.getHours()) + ":" +
-        padZero(now.getMinutes()) + ":" + padZero(now.getSeconds());
-    return res;
-}
-
-//先頭ゼロ付加
-function padZero(num) {
-    let result;
-    if (num < 10) {
-        result = "0" + num;
-    } else {
-        result = "" + num;
-    }
-    return result;
 }
